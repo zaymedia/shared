d5f3fd8199e28a10bc390d1d121f847690cd2259
Konstantin Zaytcev <zaydisk@yandex.ru>
2023-06-12 20:18:52 +0300

    add AuthenticateByKey

---
+++ src/Http/Middleware/AuthenticateByKey.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ZayMedia\Shared\Http\Middleware;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Slim\Exception\HttpUnauthorizedException;
+
+final class AuthenticateByKey implements MiddlewareInterface
+{
+    private const ATTRIBUTE = 'apiKey';
+    private string $key;
+
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    public static function findApiKey(ServerRequestInterface $request): ?string
+    {
+        /** @var string|null $value */
+        $value = $request->getAttribute(self::ATTRIBUTE);
+
+        if (\is_string($value)) {
+            return $value;
+        }
+
+        return null;
+    }
+
+    public static function getApiKey(ServerRequestInterface $request): string
+    {
+        $apiKey = self::findapiKey($request);
+
+        if ($apiKey === null) {
+            throw new HttpUnauthorizedException($request);
+        }
+
+        return $apiKey;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if (!$request->hasHeader('apiKey')) {
+            return $handler->handle($request);
+        }
+
+        $apiKey = $request->getHeaderLine('apiKey');
+
+        if (!$this->validateApiKey($apiKey)) {
+            throw new HttpUnauthorizedException($request);
+        }
+
+        return $handler->handle($request->withAttribute(self::ATTRIBUTE, $apiKey));
+    }
+
+    private function validateApiKey(string $key): bool
+    {
+        return $key !== $this->key;
+    }
+}
